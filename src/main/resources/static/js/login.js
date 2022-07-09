1790237ca8493f952689515571957e4f42d8912c
jasinthaamakara <jasinthaamakara@gmail.com>
2022-07-09 08:45:49 +0530

    add validation

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,14 +1,11 @@
 $(document).ready(function() {
+	$('#error1').hide();
 	$('#error2').hide();
-	
-	
-	 //var error=getUrlVars()["error"];
-	// if(){
-		
-//	}
-	
-	
-	
+	var error = getUrlVars()["error"];
+	if (error == "error1") {
+		$('#error1').show();
+		$('#error2').hide();
+	}
 });
 $('#submit').click(function(e) {
 	var password = $("#password").val();
@@ -18,15 +15,14 @@ $('#submit').click(function(e) {
 		$('#error2').show();
 	}
 });
-
-/* function getUrlVars() {
-          var decodedUri = decodeURIComponent(window.location.href);
-          var vars = [], hash;
-          var hashes = decodedUri.slice(decodedUri.indexOf('?') + 1).split('&');
-          for (var i = 0; i < hashes.length; i++) {
-            hash = hashes[i].split('=');
-            vars.push(hash[0]);
-            vars[hash[0]] = hash[1];
-          }
-          return vars;
-        }*/
+function getUrlVars() {
+	var decodedUri = decodeURIComponent(window.location.href);
+	var vars = [], hash;
+	var hashes = decodedUri.slice(decodedUri.indexOf('?') + 1).split('&');
+	for (var i = 0; i < hashes.length; i++) {
+		hash = hashes[i].split('=');
+		vars.push(hash[0]);
+		vars[hash[0]] = hash[1];
+	}
+	return vars;
+}
